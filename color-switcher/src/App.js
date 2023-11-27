c89a89a27a2c2fabb49fcc9264c458c0be050c40
jmatthews64 <jon.matthews64@gmail.com>
2023-11-27 18:55:05 -0500

    Created app and started initial input

---
+++ color-switcher/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const color = useState()
+  return (
+    <div className="react-root">
+      <div className='centered'>
+        <h1>Color Picker</h1>
+        <button className='red'>red</button>
+        <button className='blue'>blue</button>
+        <button className='yellow'>yellow</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
