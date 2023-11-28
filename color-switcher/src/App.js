88b37b0d8278b601b0fea31bdf8e57fd6c702881
jmatthews64 <jon.matthews64@gmail.com>
2023-11-27 19:04:58 -0500

    Added functioning buttons that change background color

--- color-switcher/src/App.js
+++ color-switcher/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
-  const color = useState()
+  const [color, setColor] = useState('')
   return (
-    <div className="react-root">
+    <div className={'react-root ' + color}>
       <div className='centered'>
         <h1>Color Picker</h1>
-        <button className='red'>red</button>
-        <button className='blue'>blue</button>
-        <button className='yellow'>yellow</button>
+        <button className='red' onClick={() => setColor('red')}>red</button>
+        <button className='blue' onClick={() => setColor('blue')}>blue</button>
+        <button className='yellow' onClick={() => setColor('yellow')}>yellow</button>
       </div>
     </div>
   );
